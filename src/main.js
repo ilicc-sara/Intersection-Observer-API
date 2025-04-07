5b8fe581948259c4df8b72f09fb8607ce86e6c92
Sara <sarailic431@gmail.com>
2025-04-07 22:03:48 +0200

    intersecting navigation

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import "./style.css";
 const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home-intro");
 
-const sectionOneOptions = {};
+// const nav = document.querySelector("nav");
+const height = header.getBoundingClientRect().height;
+
+console.log(height);
+
+const sectionOneOptions = {
+  // rootMargin: "-200px 0px 0px 0px",
+  rootMargin: `-${height}px`,
+};
 
 const sectionOneObserver = new IntersectionObserver(function (
   entries,
@@ -13,6 +21,8 @@ const sectionOneObserver = new IntersectionObserver(function (
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
     }
   });
 },
