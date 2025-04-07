028fd7764eeb9c025efbf32dd76409dcda9c889d
Sara <sarailic431@gmail.com>
2025-04-07 21:48:42 +0200

    adding classes with API

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 "use strict";
 import "./style.css";
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
